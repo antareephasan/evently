b87368af17818746d2486d384c1ce5b19eddbda8
Antareep Hasan <antareephasan@gmail.com>
2023-12-22 05:23:01 +0600

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+        <div>
+          <Link href="/">
+            <Image 
+              src="/assets/images/logo.svg"
+              alt='logo'
+              width={128}
+              height={38}
+            />
+          </Link>
+
+          <p></p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
